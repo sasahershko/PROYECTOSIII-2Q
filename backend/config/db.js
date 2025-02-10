fe6709e4845f707ca06b301221e71d469f0ea4f1
Enrique Rodriguez <enriquerodridel@gmail.com>
2025-02-10 21:10:23 +0100

    added jsonwebtoken

--- backend/config/db.js
+++ backend/config/db.js
@@ -13,7 +13,7 @@ const connectDB = async () => {
     });
     console.log("✅ Conexión a MongoDB exitosa");
   } catch (error) {
-    console.error("❌ Error al conectar a MongoDB:", error);
+    console.error("❌ Error al conectar a MongoDB:\n", error);
     process.exit(1);
   }
 };
