8766f930508e48d289e74aeda238ec297b985dd2
3noliv <enoliv02@gmail.com>
2025-02-10 21:18:19 +0100

    Solucion ejecucion del programa

--- backend/config/db.js
+++ backend/config/db.js
@@ -14,7 +14,6 @@ const connectDB = async () => {
     console.log("✅ Conexión a MongoDB exitosa");
   } catch (error) {
     console.error("❌ Error al conectar a MongoDB:\n", error);
-    process.exit(1);
   }
 };
 
