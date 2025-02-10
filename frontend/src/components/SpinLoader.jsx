b46911bb857eea86925759d83eab2a59204a3e19
Enrique Rodriguez <enriquerodridel@gmail.com>
2025-02-10 17:24:13 +0100

    Added .gitIgnore, package.json and a js script for testing MongoBD connexion

---
+++ frontend/src/components/SpinLoader.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+export default function SpinLoader({ size = "36px" }) {
+  return (
+    <div
+      style={{
+        width: size,
+        height: size,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <svg
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        transform="rotate(180)"
+        style={{
+          width: size,
+          height: size,
+          animation: "spin 1s linear infinite",
+        }}
+      >
+        <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+        <g
+          id="SVGRepo_tracerCarrier"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></g>
+        <g id="SVGRepo_iconCarrier">
+          {" "}
+          <path
+            d="M2 12C2 6.47715 6.47715 2 12 2V5C8.13401 5 5 8.13401 5 12H2Z"
+            fill="currentColor"
+          ></path>{" "}
+        </g>
+      </svg>
+      <style>
+        {`
+          @keyframes spin {
+            0% { transform: rotate(0deg); }
+            100% { transform: rotate(360deg); }
+          }
+        `}
+      </style>
+    </div>
+  );
+}
