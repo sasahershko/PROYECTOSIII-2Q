0f2e474b5ac31240d883aa258451c699876c838d
Enrique Rodriguez <enriquerodridel@gmail.com>
2025-02-11 18:29:10 +0100

    test

---
+++ frontend/src/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-2xl font-semibold">Landing</h1>
+    </div>
+  );
+}
