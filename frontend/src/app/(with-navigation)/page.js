bda9c0b17cfb7790ecfee071b49fd54a31dbc374
Enrique <enriquerodridel@gmail.com>
2025-02-14 12:34:26 +0100

    Fixed error StrokeLine

--- frontend/src/app/(with-navigation)/page.js
+++ frontend/src/app/(with-navigation)/page.js
@@ -22,11 +22,11 @@ export default function Home() {
             xmlns="http://www.w3.org/2000/svg"
             className="w-16 h-16 animate-bounce fill-copy-primary"
           >
-            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
             <g
               id="SVGRepo_tracerCarrier"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             ></g>
             <g id="SVGRepo_iconCarrier">
               {" "}
