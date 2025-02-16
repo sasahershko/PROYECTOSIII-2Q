431fc6f7d72551d64f613c5254d85ff9105fe8f4
sasahershko <sarayhershko@gmail.com>
2025-02-16 22:22:55 +0100

    Update middleware.jsx

--- frontend/src/middleware.jsx
+++ frontend/src/middleware.jsx
@@ -4,6 +4,7 @@ import { jwtVerify } from 'jose'; //para validar tokens de autenticación
 export async function middleware(req) {
     const token = req.cookies.get('token')?.value;
 
+    //si no hay token y no está en login o register, redirigir a login
     if (!token) {
         if (req.nextUrl.pathname.startsWith('/login') || req.nextUrl.pathname.startsWith('/register')) {
             return NextResponse.next();
@@ -17,22 +18,22 @@ export async function middleware(req) {
 
         const userRole = payload.rol;
 
-        // Si ya estás autenticado y en la página de login o registro, redirigir según el rol
-        if (req.nextUrl.pathname.startsWith('/login') || req.nextUrl.pathname.startsWith('/register')) {
-            if (userRole === 'admin') {
+        //si ya estás autenticado y en la página de login o register, redirigir según el rol
+        if ((req.nextUrl.pathname.startsWith('/login') || req.nextUrl.pathname.startsWith('/register'))) {
+            if (userRole === 'admin' && req.nextUrl.pathname !== '/admin') {
                 return NextResponse.redirect(new URL('/admin', req.url)); //redirigir a la página de admin si el usuario es admin
-            } else if (userRole === 'user') {
+            } else if (userRole === 'user' && req.nextUrl.pathname !== '/user') {
                 return NextResponse.redirect(new URL('/user', req.url)); //redirigir a la página de usuario si el usuario es estudiante
             }
+            return NextResponse.next(); //si ya está en su página correcta, no hacer nada
         }
 
-        //!ESTO ME DA ERROR AL MOMENTO DE ENTRAR A LA PÁGINA DE ADMIN O USER ASI QUE POR ESO DEBO HACER ESO
-        //si ya estás en la página /admin, no hacer nada
+        //si ya estás en la página /admin y eres admin, no hacer nada
         if (req.nextUrl.pathname.startsWith('/admin') && userRole === 'admin') {
             return NextResponse.next();
         }
 
-        //si ya estás en la página /user, no hacer nada
+        //si ya estás en la página /user y eres user, no hacer nada
         if (req.nextUrl.pathname.startsWith('/user') && userRole === 'user') {
             return NextResponse.next();
         }
@@ -50,8 +51,8 @@ export async function middleware(req) {
         //si todo está correcto
         return NextResponse.next();
     } catch (error) {
-        console.error('Token inválido o expirado:', error);
-        return NextResponse.redirect(new URL('/login', req.url))
+        console.error('token inválido o expirado:', error);
+        return NextResponse.redirect(new URL('/login', req.url)); //redirigir a login si el token es inválido o expiró
     }
 }
 
