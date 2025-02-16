0fc3f493709836a4071c8cdab482cae464b60901
sasahershko <sarayhershko@gmail.com>
2025-02-16 19:44:07 +0100

    He puesto un layout en admin mientras tanto

--- frontend/src/app/admin/layout.js
+++ frontend/src/app/admin/layout.js
@@ -1,9 +1,10 @@
+//TEMPORAL
 import "@/app/globals.css";
 import Footer from "@components/Footer";
 export default function RootLayout({ children }) {
 
     return (
-        <html lang="es" className={theme}>
+        <html lang="es">
             <body className="font-sans text-copy-primary">
                 {children}
                 <Footer />
