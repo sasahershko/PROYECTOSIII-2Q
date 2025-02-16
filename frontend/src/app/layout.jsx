49a9e13ed066df66054887023ed2961d5ee63bd3
sasahershko <sarayhershko@gmail.com>
2025-02-16 19:35:40 +0100

    Create layout.jsx

---
+++ frontend/src/app/layout.jsx
@@ -0,0 +1,13 @@
+import "@/app/globals.css";
+import Footer from "@components/Footer";
+export default function RootLayout({ children }) {
+
+    return (
+        <html lang="es" className={theme}>
+            <body className="font-sans text-copy-primary">
+                {children}
+                <Footer />
+            </body>
+        </html>
+    );
+}
