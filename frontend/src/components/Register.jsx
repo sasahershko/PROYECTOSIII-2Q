3006a0511545d8a306b68f48bd48175f5b0add53
Pablo Santos <pablosantoshernandez26@gmail.com>
2025-02-22 10:32:06 +0100

    rehacer Register

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,7 +1,9 @@
 "use client";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import Head from "next/head";
 import { registerUser } from "@/lib/auth";
+import Image from "next/image";
 
 export default function Register() {
   const [formData, setFormData] = useState({
@@ -29,93 +31,110 @@ export default function Register() {
       // Llamar a la función del archivo Auth.js para registrar al usuario
       const responseData = await registerUser(formData);
 
-      router.push("/login");
+      if (responseData) {
+        router.push("/login");
+      }
     } catch (error) {
       setError(error.message);
     }
   };
 
   return (
-    <div className="flex-1 flex justify-center items-center px-8 py-12">
-      <div className="bg-white p-12 rounded-lg shadow-lg w-full max-w-lg">
-        <h2 className="text-2xl font-bold text-gray-900 mb-6">Registro</h2>
-        {error && <p className="text-red-500 text-sm mb-2">{error}</p>}
+    <div className="flex w-full h-screen">
+      {/* Sección izquierda con el formulario */}
+      <div className="flex-1 bg-secundary text-white flex flex-col justify-center items-center px-8 py-12">
+        <h1 className="text-4xl font-bold mb-10">PROJECT CENTER</h1>
+        <div className="bg-white p-12 rounded-lg shadow-lg w-full max-w-lg">
+          <h2 className="text-2xl font-bold text-secundary mb-6">Registro</h2>
+          {error && <p className="text-red-500 text-sm mb-2">{error}</p>}
 
-        <form onSubmit={handleSubmit} className="space-y-6">
-          <div>
-            <input
-              type="text"
-              name="nombre"
-              value={formData.nombre}
-              onChange={(e) =>
-                setFormData({ ...formData, nombre: e.target.value })
-              }
-              className="w-full px-4 py-3 rounded-md text-gray-900 bg-gray-100 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder-gray-500"
-              placeholder="Nombre y Apellidos"
-              required
-            />
-          </div>
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div>
+              <input
+                type="text"
+                name="nombre"
+                value={formData.nombre}
+                onChange={(e) =>
+                  setFormData({ ...formData, nombre: e.target.value })
+                }
+                className="w-full px-4 py-3 rounded-md text-secundary bg-gray-100 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder-gray-500"
+                placeholder="Nombre y Apellidos"
+                required
+              />
+            </div>
 
-          <div>
-            <input
-              type="email"
-              name="correo"
-              value={formData.correo}
-              onChange={(e) =>
-                setFormData({ ...formData, correo: e.target.value })
-              }
-              className="w-full px-4 py-3 rounded-md text-gray-900 bg-gray-100 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder-gray-500"
-              placeholder="Correo Electrónico"
-              required
-            />
-          </div>
+            <div>
+              <input
+                type="email"
+                name="correo"
+                value={formData.correo}
+                onChange={(e) =>
+                  setFormData({ ...formData, correo: e.target.value })
+                }
+                className="w-full px-4 py-3 rounded-md text-secundary bg-gray-100 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder-gray-500"
+                placeholder="Correo Electrónico"
+                required
+              />
+            </div>
 
-          <div>
-            <input
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={(e) =>
-                setFormData({ ...formData, password: e.target.value })
-              }
-              className="w-full px-4 py-3 rounded-md text-gray-900 bg-gray-100 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder-gray-500"
-              placeholder="Contraseña"
-              required
-            />
-          </div>
+            <div>
+              <input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={(e) =>
+                  setFormData({ ...formData, password: e.target.value })
+                }
+                className="w-full px-4 py-3 rounded-md text-secundary bg-gray-100 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder-gray-500"
+                placeholder="Contraseña"
+                required
+              />
+            </div>
 
-          <div>
-            <select
-              name="grado"
-              value={formData.grado}
-              onChange={(e) =>
-                setFormData({ ...formData, grado: e.target.value })
-              }
-              className="w-full px-4 py-3 rounded-md text-gray-900 bg-gray-100 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
-              required
+            <div>
+              <select
+                name="grado"
+                value={formData.grado}
+                onChange={(e) =>
+                  setFormData({ ...formData, grado: e.target.value })
+                }
+                className="w-full px-4 py-3 rounded-md text-secundary bg-gray-100 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                required
+              >
+                {gradosPermitidos.map((grado, index) => (
+                  <option key={index} value={grado}>
+                    {grado}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            <button
+              type="submit"
+              className="w-full bg-gray-800 text-white py-3 rounded-lg font-semibold hover:bg-gray-700"
             >
-              {gradosPermitidos.map((grado, index) => (
-                <option key={index} value={grado}>
-                  {grado}
-                </option>
-              ))}
-            </select>
-          </div>
+              Registrarse
+            </button>
+          </form>
 
-          <button
-            type="submit"
-            className="w-full bg-gray-800 text-white py-3 rounded-lg font-semibold hover:bg-gray-700"
-          >
-            Registrarse
-          </button>
-        </form>
+          <p className="text-gray-600 text-sm mt-6 text-center">
+            ¿Ya tienes cuenta?{" "}
+            <a href="/login" className="text-blue-500 font-semibold">
+              Inicia sesión
+            </a>
+          </p>
+        </div>
+      </div>
 
-        <p className="text-gray-600 text-sm mt-6 text-center">
-          ¿Ya tienes cuenta?{" "}
-          <a href="/login" className="text-blue-500 font-semibold">
-            Inicia sesión
-          </a>
-        </p>
+      {/* Sección derecha con imagen */}
+      <div className="flex-1 bg-gray-300 flex items-center justify-center">
+        <Image
+          src="/foto-auth.webp"
+          width={1100}
+          height={1200}
+          className="h-screen"
+          alt="Imagen de inicio de sesión"
+        />
       </div>
     </div>
   );
