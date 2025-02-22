f30309e32e59426e6c7157c213432112d041210f
Pablo Santos <pablosantoshernandez26@gmail.com>
2025-02-22 10:32:55 +0100

    Rehacer layout

--- frontend/src/app/(auth)/layout.js
+++ frontend/src/app/(auth)/layout.js
@@ -1,36 +1,17 @@
 import "@/app/globals.css";
 import Link from "next/link";
-import Image from "next/image";
 
 export default function AuthLayout({ children }) {
   return (
     <html className="dark">
-      <body className="font-sans text-copy-primary">
-        {/* Enlace a la página principal */}
+      <body className="font-sans text-primary-text">
         <Link
           href="/"
           className="absolute text-white top-0 left-0 p-4 hover:opacity-85 py-5 px-7 font-bold"
         >
           ⭠ HOME
         </Link>
-
-        <div className="flex w-full min-h-screen bg-gray-900">
-          {/* Sección izquierda con el formulario */}
-          <div className="flex-1 items-center justify-center">
-            {children} {/* Formularios de Login y Register */}
-          </div>
-
-          {/* Sección derecha con imagen */}
-          <div className="hidden md:flex flex-1 bg-gray-300 items-center justify-center">
-            <Image
-              src="/foto-auth.webp"
-              width={1100}
-              height={1200}
-              className="h-screen"
-              alt="Imagen de autenticación"
-            />
-          </div>
-        </div>
+        {children}
       </body>
     </html>
   );
