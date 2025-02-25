a0cdf005e538d3f58bad9beeabcd230819c55f98
Dek3n <148790347+Dek3n@users.noreply.github.com>
2025-02-25 19:28:07 +0100

    Update User.js
    Se agregan los campos necesarios para la verificación con código de 6 dígitos:
    isVerified: Indica si el usuario ya verificó su cuenta.
    verificationCode: Almacena el código temporal de verificación.
    verificationAttempts: Controla cuántos intentos tiene el usuario.

--- backend/models/User.js
+++ backend/models/User.js
@@ -18,6 +18,11 @@ const userSchema = new mongoose.Schema({
     enum: ["INSO", "MAIS", "FIIS", "DIPI", "ANIV"], // Solo permite estos valores
   },
   projects: [{ type: mongoose.Schema.Types.ObjectId, ref: "Project" }], //relacion inversa
+
+  //Campos para la veriificacion del codigo de 6 digitos
+  isVerified:{type: Boolean, default: false },//Indica si el usuario ya verificó su cuenta
+  verificationCode:{ type: String, default:null},//Codigo temporal de verificacion
+  verificationAttempts:{ type:Number, default:3}//numero de intentos para ingresar el codigo
 });
 
 const User = mongoose.model("User", userSchema);
