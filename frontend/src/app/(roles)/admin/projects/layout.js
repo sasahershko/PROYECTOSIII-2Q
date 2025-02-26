5a37e1ff5e228965fadf01fd976e2032c17b8818
Enrique <enriquerodridel@gmail.com>
2025-02-26 20:32:11 +0100

    Started in users and changed a bit themes

--- frontend/src/app/(roles)/admin/projects/layout.js
+++ frontend/src/app/(roles)/admin/projects/layout.js
@@ -1,13 +1,12 @@
 // app/layout.js
 import "@/app/globals.css";
-import Footer from "@components/Footer";
 import AdminNavBar from "@/components/admin/ProjectsNavBar";
 
 export default function RootLayout({ children }) {
-    return (
-        <div>
-            <AdminNavBar />
-            <div className="w-auto h-auto bg-primary-bg">{children}</div>
-        </div>
-    );
+  return (
+    <div>
+      <AdminNavBar />
+      <div className="w-auto h-auto bg-primary-bg">{children}</div>
+    </div>
+  );
 }
