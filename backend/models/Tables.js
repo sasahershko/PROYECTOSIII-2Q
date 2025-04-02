30015ddb73ef2e856c69b6bb408513ee96e4a7ea
Diego740 <57465270+Diego740@users.noreply.github.com>
2025-04-02 18:23:26 +0200

    Update Tables.js

--- backend/models/Tables.js
+++ backend/models/Tables.js
@@ -2,7 +2,9 @@ const tableSchema = new mongoose.Schema({
     number: { type: Number, required: true, unique: true },
     zone: { type: String, required: true },
     capacity: { type: Number, required: true, min: 1 },
-  });
+  },
+  { timestamps: true }
+);
   
   const Table = mongoose.model("Table", tableSchema);
   export default Table;
