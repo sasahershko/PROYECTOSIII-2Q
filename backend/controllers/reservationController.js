a4f825cdb2ae1d37774c85f718ac98bdd4a1ccf7
3noliv <enoliv02@gmail.com>
2025-04-22 01:44:02 +0200

    handleHttpError en reservas e ideas

--- backend/controllers/reservationController.js
+++ backend/controllers/reservationController.js
@@ -1,7 +1,7 @@
 import Reservation from "../models/Reservation.js";
 import Table from "../models/Tables.js";
 import Project from "../models/Project.js";
-
+import { handleHttpError } from "../utils/handleHttpError.js";
 
 //CREAR UNA RESERVA
 export const createReservation = async (req, res) => {
@@ -11,17 +11,15 @@ export const createReservation = async (req, res) => {
 
     // Verificar si la mesa existe
     const existingTable = await Table.findById(table);
-    if (!existingTable) {
-      return res.status(404).json({ message: "Mesa no encontrada." });
-    }
-
+    if (!existingTable) return handleHttpError(res, "Mesa no encontrada.", 404);
 
     // Verificar si el proyecto existe y pertenece al usuario
-    const existingProject = await Project.findOne({ _id: project, members: userId });
-    if (!existingProject) {
-      return res.status(403).json({ message: "No tienes acceso a este proyecto." });
-    }
-
+    const existingProject = await Project.findOne({
+      _id: project,
+      members: userId,
+    });
+    if (!existingProject)
+      return handleHttpError(res, "No tienes acceso a este proyecto.", 403);
 
     // Verificar disponibilidad de la mesa en la franja horaria
     const overlappingReservation = await Reservation.findOne({
@@ -29,15 +27,18 @@ export const createReservation = async (req, res) => {
       date,
       $or: [
         { startTime: { $lt: endTime, $gte: startTime } }, // Se solapa al inicio
-        { endTime: { $gt: startTime, $lte: endTime } },   // Se solapa al final
+        { endTime: { $gt: startTime, $lte: endTime } }, // Se solapa al final
         { startTime: { $lte: startTime }, endTime: { $gte: endTime } }, // Contiene la reserva completamente
       ],
     });
 
     //if (overlappingReservation && overlappingReservation.status != "rejected") {
-    if (overlappingReservation) {
-      return res.status(400).json({ message: "La mesa ya está reservada en este horario." });
-    }
+    if (overlappingReservation)
+      return handleHttpError(
+        res,
+        "La mesa ya está reservada en este horario.",
+        400
+      );
 
     // Crear la reserva
     const newReservation = new Reservation({
@@ -51,14 +52,14 @@ export const createReservation = async (req, res) => {
     });
 
     await newReservation.save();
-    res.status(201).json({ message: "Reserva creada con éxito.", reservation: newReservation });
-
+    res.status(201).json({
+      message: "Reserva creada con éxito.",
+      reservation: newReservation,
+    });
   } catch (error) {
-    //console.error(error);
-    res.status(500).json({ message: "Error al crear la reserva." });
+    handleHttpError(res, error);
   }
 };
-
 
 export const getUserReservations = async (req, res) => {
   try {
@@ -66,80 +67,66 @@ export const getUserReservations = async (req, res) => {
 
     const reservations = await Reservation.find({ user: userId });
 
-    if (reservations.length === 0) {
-      return res.status(404).json({ message: "No se encontraron reservas." });
-    }
-
+    if (reservations.length === 0)
+      return handleHttpError(res, "No se encontraron reservas.", 404);
     res.status(200).json(reservations);
   } catch (error) {
-    //console.error(error);
-    res.status(500).json({ message: "Error al obtener las reservas del usuario." });
+    handleHttpError(res, error);
   }
 };
 
 //probar find().populate("table", "number zone capacity")
 export const getAllReservations = async (req, res) => {
   try {
-
     const reservations = await Reservation.find();
 
-    if (reservations.length === 0) {
-      return res.status(404).json({ message: "No se encontraron reservas." });
-    }
+    if (reservations.length === 0)
+      return handleHttpError(res, "No se encontraron reservas.", 404);
 
     // Devolver las reservas encontradas
     res.status(200).json(reservations);
   } catch (error) {
-    //console.error(error);
-    res.status(500).json({ message: "Error al obtener las reservas." });
+    handleHttpError(res, error);
   }
 };
-
 
 export const approveReservation = async (req, res) => {
   try {
     const { id } = req.params;
     const reservation = await Reservation.findById(id);
 
-    if (!reservation) {
-      return res.status(404).json({ message: "Reserva no encontrada." });
-    }
-
-    if (reservation.status === "approved") {
-      return res.status(400).json({ message: "La reserva ya está aprobada." });
-    }
+    if (!reservation)
+      return handleHttpError(res, "Reserva no encontrada.", 404);
+    if (reservation.status === "approved")
+      return handleHttpError(res, "La reserva ya está aprobada.", 400);
 
     reservation.status = "approved";
     await reservation.save();
 
-    res.status(200).json({ message: "Reserva aprobada con éxito.", reservation });
+    res
+      .status(200)
+      .json({ message: "Reserva aprobada con éxito.", reservation });
   } catch (error) {
-    //console.error(error);
-    res.status(500).json({ message: "Error al aceptar la reserva." });
+    handleHttpError(res, error);
   }
 };
-
 
 export const rejectReservation = async (req, res) => {
   try {
     const { id } = req.params;
     const reservation = await Reservation.findById(id);
-
-    if (!reservation) {
-      return res.status(404).json({ message: "Reserva no encontrada." });
-    }
-
-    if (reservation.status === "rejected") {
-      return res.status(400).json({ message: "La reserva ya está rechazada." });
-    }
+    if (!reservation)
+      return handleHttpError(res, "Reserva no encontrada.", 404);
+    if (reservation.status === "rejected")
+      return handleHttpError(res, "La reserva ya está rechazada.", 400);
 
     reservation.status = "rejected";
     await reservation.save();
-
-    res.status(200).json({ message: "Reserva rechazada con éxito.", reservation });
+    res
+      .status(200)
+      .json({ message: "Reserva rechazada con éxito.", reservation });
   } catch (error) {
-    //console.error(error);
-    res.status(500).json({ message: "Error al rechazar la reserva." });
+    handleHttpError(res, error);
   }
 };
 
@@ -148,30 +135,35 @@ export const deleteReservation = async (req, res) => {
     const { id } = req.params;
     const { type } = req.query;
 
-
     const reservation = await Reservation.findById(id);
 
-    if (!reservation) {
-      return res.status(404).json({ message: "Reserva no encontrada." });
-    }
+    if (!reservation)
+      return handleHttpError(res, "Reserva no encontrada.", 404);
 
-    if (req.usuario.rol !== "admin" && reservation.user.toString() !== req.usuario._id.toString()) {
-      return res.status(403).json({ message: "No tienes permisos para eliminar esta reserva." });
+    if (
+      req.usuario.rol !== "admin" &&
+      reservation.user.toString() !== req.usuario._id.toString()
+    ) {
+      return handleHttpError(
+        res,
+        "No tienes permisos para eliminar esta reserva.",
+        403
+      );
     }
 
     if (type === "hard") {
       await reservation.deleteOne();
-      return res.status(200).json({ message: "Reserva eliminada permanentemente." });
-    } 
+      return res
+        .status(200)
+        .json({ message: "Reserva eliminada permanentemente." });
+    }
 
     // Soft delete por defecto
     await reservation.delete();
-    return res.status(200).json({ message: "Reserva marcada como eliminada (soft delete)." });
-
+    res
+      .status(200)
+      .json({ message: "Reserva marcada como eliminada (soft delete)." });
   } catch (error) {
-    //console.error(error);
-    res.status(500).json({ message: "Error al eliminar la reserva." });
+    handleHttpError(res, error);
   }
 };
-
-
