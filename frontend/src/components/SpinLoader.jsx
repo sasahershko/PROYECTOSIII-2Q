411d3cad9f59ed3187eabdae233489e74a73a8d3
Enrique <enriquerodridel@gmail.com>
2025-05-11 20:38:22 +0200

    fixed SpinLoader style and colors

--- frontend/src/components/SpinLoader.jsx
+++ frontend/src/components/SpinLoader.jsx
@@ -1,49 +1,32 @@
 // components/SpinLoader.jsx
 import React from "react";
 
-export default function SpinLoader({ size = "36px" }) {
+export default function SpinLoader({
+  size = "36px",
+  className = "text-primary-text",
+}) {
   return (
     <div
       style={{
         width: size,
         height: size,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
       }}
+      className={`flex justify-center items-center ${className}`}
     >
       <svg
         viewBox="0 0 16 16"
         xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        style={{
-          width: "100%",
-          height: "100%",
-          animation: "spin 1s linear infinite",
-        }}
+        fill="currentColor"
+        className="w-full h-full animate-spin"
       >
-        <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-        <g
-          id="SVGRepo_tracerCarrier"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        ></g>
-        <g id="SVGRepo_iconCarrier">
-          <g fill="#000000" fillRule="evenodd" clipRule="evenodd">
-            <path
-              d="M8 1.5a6.5 6.5 0 100 13 6.5 6.5 0 000-13zM0 8a8 8 0 1116 0A8 8 0 010 8z"
-              opacity=".2"
-            ></path>
-            <path d="M7.25.75A.75.75 0 018 0a8 8 0 018 8 .75.75 0 01-1.5 0A6.5 6.5 0 008 1.5a.75.75 0 01-.75-.75z"></path>
-          </g>
-        </g>
+        {/* Background circle with low opacity */}
+        <path
+          d="M8 1.5a6.5 6.5 0 100 13 6.5 6.5 0 000-13zM0 8a8 8 0 1116 0A8 8 0 010 8z"
+          fillOpacity="0.2"
+        />
+        {/* Foreground arc */}
+        <path d="M7.25.75A.75.75 0 018 0a8 8 0 018 8 .75.75 0 01-1.5 0A6.5 6.5 0 008 1.5a.75.75 0 01-.75-.75z" />
       </svg>
-      <style>
-        {`@keyframes spin {
-            0% { transform: rotate(0deg); }
-            100% { transform: rotate(360deg); }
-          }`}
-      </style>
     </div>
   );
 }
